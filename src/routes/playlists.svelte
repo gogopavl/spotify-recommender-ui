53195682e939b424cb193b735ff9120856383de2
gogopavl <pgogousis@gmail.com>
2021-12-13 00:52:00 +0000

    switch over to sveltekit

---
+++ src/routes/playlists.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { playlists } from "../stores/playlists";
+</script>
+
+<svelte:head>
+  <title>Spotify Playlist Recommender - Playlists</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">Playlists</h1>
+
+{#each $playlists as playlist}
+  <p>{playlist.name}</p>
+  <img
+    src={playlist.image}
+    alt="{playlist.name} playlist cover"
+    width="200px"
+  />
+{/each}
